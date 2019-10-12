35a1942bef292e0f966b1489d9980c4e85f1f75f
louchenyao <louchenyao@gmail.com>
2019-10-12 17:13:25 +0800

    abstract sepset into a class

--- ssfe_benchmarks/bench.cpp
+++ ssfe_benchmarks/bench.cpp
@@ -1,92 +1,68 @@
-#include <rte_common.h>
-#include <rte_memory.h>
-#include <rte_launch.h>
-#include <rte_eal.h>
-#include <rte_efd.h>
-#include <rte_per_lcore.h>
-#include <rte_lcore.h>
-#include <rte_cycles.h>
-#include <rte_timer.h>
-#include <rte_debug.h>
+#include "sepset.h"
 
-#include <cassert>
 #include <chrono>
 #include <map>
 
-void init_rte_eal() {
-    char *argv[3];
-    char a0[] = "./bench";
-    char a1[] = "--no-huge";
-    char a2[] = "-m256"; // reserve 256 MB memory
-    argv[0] = a0;
-    argv[1] = a1;
-    argv[2] = a2;
-    int ret = rte_eal_init(3, argv);
-    if (ret < 0)
-        rte_panic("Cannot init EAL\n");
+template <typename KEY_TYPE>
+void insert_generated_kvs(SepSet<KEY_TYPE> &t, int n) {
+    for (int i = 0; i < n; i++) {
+        t.update(i, rand()%2);
+    }
 }
 
-efd_value_t bench_lookup(struct rte_efd_table *table, int n) {
+efd_value_t bench_lookup(int n) {
     printf("benchmaking lookup...\n");
+    auto t = SepSet<uint64_t>(n);
+    insert_generated_kvs(t, n);
+
     efd_value_t x = 0;
     auto start = std::chrono::high_resolution_clock::now();
     int cnt = 0;
     for (int round = 0; round < 10; ++round) {
         for (int i = 0; i < n; i++) {
             cnt++;
-            uint64_t key = i;
-            x ^= rte_efd_lookup(table, 0, &key);
+            x ^= t.query(i);
         }
     }
     auto end = std::chrono::high_resolution_clock::now();
-    double ns = double(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()) / cnt;
+    double ns =
+        double(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+                   .count()) /
+        cnt;
     printf("time/lookup = %.3lf ns\n", ns);
     return x;
 }
 
-void bench_lookup_bulk(struct rte_efd_table *table, int n) {
+void bench_lookup_bulk(int n) {
     printf("benchmaking lookup_bulk...\n");
+    auto t = SepSet<uint64_t>(n);
+    insert_generated_kvs(t, n);
+
     uint64_t keys[16];
-    void *key_list[16];
-    for (int i = 0; i < 16; i++) {
-        key_list[i] = (void *)&keys[i];
-    }
-    efd_value_t val[16];
+    bool val[16];
 
     auto start = std::chrono::high_resolution_clock::now();
     int cnt = 0;
     for (int round = 0; round < 10; round++) {
         for (int i = 0; i < n - 16; i += 16) {
             cnt++;
-            for (int j = 0; j < 16; j ++) {
+            for (int j = 0; j < 16; j++) {
                 keys[j] = i + j;
             }
-            rte_efd_lookup_bulk(table, 0, 16, (const void **)key_list, val);
+            t.query_batch(keys, val, 16);
         }
     }
     auto end = std::chrono::high_resolution_clock::now();
-    auto ns = double(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()) / cnt;
+    auto ns =
+        double(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+                   .count()) /
+        cnt;
     printf("time/16_lookups = %.3lf ns\n", ns);
-
 }
 
 int main() {
-    init_rte_eal();
-
     int n = 2000000;
-    auto *table = rte_efd_create("hi", n, 64/8, 1, 1);
-    assert(table != nullptr);
-
-    for (int i = 0; i < n; i++) {
-        uint64_t key = i;
-        efd_value_t val = (uint8_t)i % 2;
-        assert(rte_efd_update(table, 0, &key, val) == 0);
-    }
-
-    printf("\n");
-    bench_lookup(table, n);
-    bench_lookup_bulk(table, n);
-
-    rte_efd_free(table);
+    bench_lookup(n);
+    bench_lookup_bulk(n);
     return 0;
 }
---
+++ ssfe_benchmarks/sepset.h
@@ -0,0 +1,61 @@
+#include <rte_common.h>
+#include <rte_cycles.h>
+#include <rte_debug.h>
+#include <rte_eal.h>
+#include <rte_efd.h>
+#include <rte_launch.h>
+#include <rte_lcore.h>
+#include <rte_memory.h>
+#include <rte_per_lcore.h>
+#include <rte_timer.h>
+
+#include <cassert>
+#include <string>
+
+void init_rte_eal() {
+    static bool initialized = false;
+    if (initialized) return;
+    initialized = true;
+
+    char *argv[3];
+    char a0[] = "./bench";
+    char a1[] = "--no-huge";
+    char a2[] = "-m256";  // reserve 256 MB memory
+    argv[0] = a0;
+    argv[1] = a1;
+    argv[2] = a2;
+    int ret = rte_eal_init(3, argv);
+    if (ret < 0) rte_panic("Cannot init EAL\n");
+}
+
+template <typename KEY_TYPE>
+class SepSet {
+   public:
+    SepSet(int max_capacity) {
+        init_rte_eal();
+        table_ = rte_efd_create(("sepset_" + std::to_string(rand())).c_str(),
+                                max_capacity, sizeof(KEY_TYPE), 1, 1);
+        assert(table_ != nullptr);
+    }
+
+    ~SepSet() { rte_efd_free(table_); }
+
+    inline void update(KEY_TYPE key, bool val) {
+        assert(rte_efd_update(table_, 0, &key, (efd_value_t)val) == 0);
+    }
+
+    inline bool query(KEY_TYPE key) {
+        return (bool)rte_efd_lookup(table_, 0, &key);
+    }
+
+    inline void query_batch(KEY_TYPE *keys, bool *res, int batch_size) {
+        void *key_list[16];
+        assert(batch_size <= 16);
+        for (int i = 0; i < batch_size; i++) {
+            key_list[i] = keys + i;
+        }
+        rte_efd_lookup_bulk(table_, 0, batch_size, (const void **)key_list, (efd_value_t *)res);
+    }
+   private:
+    rte_efd_table *table_;
+};
