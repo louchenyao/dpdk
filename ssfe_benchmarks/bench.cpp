d681561867738f49ab49cfd1de4d106749fe65b5
louchenyao <louchenyao@gmail.com>
2019-10-15 10:30:23 +0800

    add update/build benchmarks

--- ssfe_benchmarks/bench.cpp
+++ ssfe_benchmarks/bench.cpp
@@ -12,6 +12,26 @@ void insert_generated_kvs(SepSet<KEY_TYPE> &t, int n) {
     }
 }
 
+static void BM_sepset_update(benchmark::State& state) {
+    auto t = SepSet<uint64_t>(state.range(0));
+    int x = 0;
+    for (int i = 0; i < state.range(0); i++) {
+        t.update(i, (i%2)^x);
+    }
+    x ^= 1;
+
+    // Benchmark
+    int i = 0;
+    for (auto _ : state) {
+        t.update(i, (i%2)^x);
+        i = i + 1;
+        if (i == state.range(0)) {
+            i = 0;
+            x ^= 1;
+        }
+    }
+}
+
 static void BM_sepset_query(benchmark::State& state) {
     auto t = SepSet<uint64_t>(state.range(0));
     insert_generated_kvs(t, state.range(0));
@@ -25,6 +45,23 @@ static void BM_sepset_query(benchmark::State& state) {
             i = 0;
         }
     }
+}
+
+
+static void BM_sepset_build(benchmark::State& state) {
+    auto t = SepSet<uint64_t>(state.range(0));
+
+    // Benchmark
+    int x = 0;
+    for (auto _ : state) {
+        auto t = SepSet<uint64_t>(state.range(0));
+        for (int i = 0; i < state.range(0); i++) {
+            t.update(i, (i%2)^x);
+        }
+        x^=0;
+    }
+
+    state.SetItemsProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
 }
 
 static void BM_sepset_query_batch(benchmark::State& state) {
@@ -48,5 +85,7 @@ static void BM_sepset_query_batch(benchmark::State& state) {
     }
 }
 
+BENCHMARK(BM_sepset_build)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
 BENCHMARK(BM_sepset_query)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
 BENCHMARK(BM_sepset_query_batch)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
+BENCHMARK(BM_sepset_update)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
